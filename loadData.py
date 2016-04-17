c350a8fb8a0bc7046c26a0ccf2d0b98173e99085
FTD_Azzinoth <zshwang0809@gmail.com>
2016-04-17 01:24:56 -0500

    first commit

---
+++ loadData.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import json
+import requests
+
+# Connect to database
+client = MongoClient()
+db = client.uber_challenge
+coll = db.film
+
+# Remove old data before getting new data
+coll.remove()
+
+# Dump the data from provided API endpoint
+r = requests.get('https://data.sfgov.org/resource/yitu-d5am.json')
+data = json.loads(r.text)
+
+for i in range(len(data)):
+    curr = data[i]
+    # Remove unnecessary field
+    if curr.has_key('smile_again_jenny_lee') == True:
+        del curr['smile_again_jenny_lee']
+    # # Insert manual id field for url query
+    curr['id'] = i + 1
+    # Insert into database
+    coll.insert_one(curr)
